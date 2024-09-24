7291a39de8767da9ce1e80e5c7e3afb780862131
yashgoyal0110 <149111979+yashgoyal0110@users.noreply.github.com>
2024-09-24 15:07:31 +0530

    Context.jsx

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,57 +13,75 @@ const ContextProvider = (props) => {
     const [resultData, setResultData] = useState("")
 
     const delayPara = (index, nextWord) => {
-        setTimeout(function (){
-            setResultData(prev => prev+nextWord)
-        }, 100*index)
+        setTimeout(() => {
+            setResultData(prev => prev + nextWord);
+        }, 100 * index);
     }
-    
-    const onSent = async(prompt) => {
+
+    const onSent = async (prompt) => {
         setResultData("");
         setLoading(true);
         setShowResult(true);
-        // setRecentPrompt(input); 
+        
         let response;
         if (prompt !== undefined) {
             response = await run(prompt);
             setRecentPrompt(prompt);
-        }
-        else{
-            setPrevPrompts(prev => [...prev, input])
-            setRecentPrompt(input)
+        } else {
+            setPrevPrompts(prev => [...prev, input]);
+            setRecentPrompt(input);
             response = await run(input);
         }
 
-        setPrevPrompts(prev => [...prev, prompt])
-        
-       let responseArray = response.split("**");
-       let newResponse = "";
-       for(let i = 0; i < responseArray.length; i++) {
-        if (i === 0 || i % 2 !== 1) {
-            newResponse += responseArray[i];
-        }
-        else{
-            newResponse += "<b>" + responseArray[i] + "</b>"
-        }
-       }
-       let newResponse2 = newResponse.split("*").join("</br>")
-       let newResponseArray = newResponse2.split(" ");
-       for (let i = 0; i < newResponseArray.length; i++) {
-        const nextWord = newResponseArray[i];
-        delayPara(i, nextWord+" ");
-       }
-       setResultData(newResponse2);
-       setLoading(false);
-       setInput("");
+        setPrevPrompts(prev => [...prev, prompt || input]);
 
+        let responseArray = response.split("**");
+        let newResponse = "";
+
+
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+
+
+        let newResponseWithBreaks = newResponse.split("*").join("<br/>");
+
+
+        let newResponseArray = newResponseWithBreaks.split(" ");
+        let newResponseFinal = "";
+        
+        for (let i = 0; i < newResponseArray.length; i++) {
+            let nextWord = newResponseArray[i];
+
+
+            if (nextWord === "-") {
+                newResponseFinal += "<li>";
+            } else if (nextWord === "<br/>") {
+                newResponseFinal += "<br/>";
+            } else {
+                newResponseFinal += nextWord + " ";
+            }
+
+            if (i > 0 && newResponseArray[i - 1] === "-") {
+                newResponseFinal += "</li>";
+            }
+
+            delayPara(i, nextWord + " ");
+        }
+
+        setLoading(false);
+        setInput("");
     }
 
     const newChat = () => {
         setInput("");
         setResultData("");
-        setPrevPrompts([]);
         setRecentPrompt("");
-      };
+    }
 
     const contextValue = {
         prevPrompts,
@@ -86,4 +104,4 @@ const ContextProvider = (props) => {
     )
 }
 
-export default ContextProvider
+export default ContextProvider;
